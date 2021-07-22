9347cb4c3a1a6dcad70b8d853a8213e28fed4763
ZhuoyunZhong <zzhong3@wpi.edu>
2021-07-22 18:23:20 -0400

    add unity-ros communication

--- gopher_unity/gopher_unity_endpoint/src/server_endpoint.py
+++ gopher_unity/gopher_unity_endpoint/src/server_endpoint.py
@@ -4,8 +4,8 @@ import rospy
 from ros_tcp_endpoint import TcpServer, RosPublisher, RosSubscriber, RosService
 
 from std_msgs.msg import Float64
-from geometry_msgs.msg import Pose, Twist
-from sensor_msgs.msg import CompressedImage, LaserScan
+from geometry_msgs.msg import Pose, Twist, PoseStamped, TwistStamped
+from sensor_msgs.msg import CompressedImage, LaserScan, JointState
 
 # from niryo_moveit.srv import MoverService, MoverServiceRequest
 
@@ -18,9 +18,10 @@ def main():
     # Start the Server Endpoint with a ROS communication objects dictionary for routing messages
     tcp_server.start({
         # Ground truth
-        "model_pose": RosPublisher("model_pose", Pose, queue_size=1),
-        "model_vel": RosPublisher("model_vel", Twist, queue_size=1),
-        # Sensor
+        "model_pose": RosPublisher("model_pose", PoseStamped, queue_size=1),
+        "model_twist": RosPublisher("model_twist", TwistStamped, queue_size=1),
+        
+        # Sensors
         # camera
         "main_cam/color/image_raw/compressed": 
             RosPublisher("main_cam/color/image_raw/compressed", CompressedImage, queue_size=1),
@@ -30,7 +31,52 @@ def main():
             RosPublisher("left_arm_cam/color/image_raw/compressed", CompressedImage, queue_size=1),
         # lidar
         "base_scan": 
-            RosPublisher("base_scan", LaserScan, queue_size=1)
+            RosPublisher("base_scan", LaserScan, queue_size=1),
+        
+        # State
+        "joint_states": 
+            RosPublisher("joint_states", JointState, queue_size=1),
+        
+        # Controllers
+        # base controller
+        "base_controller/cmd_vel":
+            RosSubscriber("base_controller/cmd_vel", Twist, tcp_server),
+        # camera controller
+        "main_cam_pitch_controller/command":
+            RosSubscriber("main_cam_pitch_controller/command", Float64, tcp_server),
+        "main_cam_yaw_controller/command":
+            RosSubscriber("main_cam_yaw_controller/command", Float64, tcp_server),
+        # joint controllers
+        "joint_1_position_controller/command":
+            RosSubscriber("right_arm_joint_1_position_controller/command", Float64, tcp_server),
+        "joint_2_position_controller/command":
+            RosSubscriber("right_arm_joint_2_position_controller/command", Float64, tcp_server),
+        "joint_3_position_controller/command":
+            RosSubscriber("right_arm_joint_3_position_controller/command", Float64, tcp_server),
+        "joint_4_position_controller/command":
+            RosSubscriber("right_arm_joint_4_position_controller/command", Float64, tcp_server),
+        "joint_5_position_controller/command":
+            RosSubscriber("right_arm_joint_5_position_controller/command", Float64, tcp_server),
+        "joint_6_position_controller/command":
+            RosSubscriber("right_arm_joint_6_position_controller/command", Float64, tcp_server),
+        "joint_7_position_controller/command":
+            RosSubscriber("right_arm_joint_7_position_controller/command", Float64, tcp_server),
+        "joint_1_position_controller/command":
+            RosSubscriber("left_arm_joint_1_position_controller/command", Float64, tcp_server),
+        "joint_2_position_controller/command":
+            RosSubscriber("left_arm_joint_2_position_controller/command", Float64, tcp_server),
+        "joint_3_position_controller/command":
+            RosSubscriber("left_arm_joint_3_position_controller/command", Float64, tcp_server),
+        "joint_4_position_controller/command":
+            RosSubscriber("left_arm_joint_4_position_controller/command", Float64, tcp_server),
+        "joint_5_position_controller/command":
+            RosSubscriber("left_arm_joint_5_position_controller/command", Float64, tcp_server),
+        "joint_6_position_controller/command":
+            RosSubscriber("left_arm_joint_6_position_controller/command", Float64, tcp_server),
+        "joint_7_position_controller/command":
+            RosSubscriber("left_arm_joint_7_position_controller/command", Float64, tcp_server)
+
+        # 'niryo_moveit': RosService('niryo_moveit', MoverService)
     })
     
     rospy.spin()
