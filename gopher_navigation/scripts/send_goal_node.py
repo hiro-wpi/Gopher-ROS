9ffa60c0f1d0513cf04b40c0ab7fcaa95afe4544
Hoang Nguyen <hcnguyen@wpi.edu>
2023-05-07 12:24:04 -0400

    we are able to send a goal anywhere on the map

--- gopher_navigation/scripts/send_goal_node.py
+++ gopher_navigation/scripts/send_goal_node.py
@@ -6,23 +6,27 @@ import roslib
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from std_srvs.srv import Empty, EmptyResponse
+from nav_msgs.srv import GetPlan, GetPlanRequest, GetPlanResponse
 
 
     
 
-def movebase_client():
+def movebase_client(goalRequested):
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
     client.wait_for_server()
 
     goal = MoveBaseGoal()
     # fill in our goal msg
-    
-    goal.target_pose.header.frame_id = 'map'
-    goal.target_pose.header.stamp = rospy.Time.now()
 
-    # Move forward 2 meter
-    goal.target_pose.pose.position.x = 6.0
-    goal.target_pose.pose.orientation.w = 1.0
+    goal.target_pose.header =   goalRequested.header
+    goal.target_pose.pose =     goalRequested.pose
+    
+    # goal.target_pose.header.frame_id = 'map'
+    # goal.target_pose.header.stamp = rospy.Time.now()
+
+    # # Move forward 2 meter
+    # goal.target_pose.pose.position.x = 6.0
+    # goal.target_pose.pose.orientation.w = 1.0
 
     client.send_goal(goal)
     # client.cancel_goal()
@@ -37,17 +41,20 @@ def movebase_client():
         # client.get_state()
 
 def send_goal(req):
-    result = movebase_client()
+    
+    goal = req.goal
+
+    result = movebase_client(goal)
     if result:
         rospy.loginfo("Goal Execution Done")
-    return EmptyResponse()
+    return GetPlanResponse()
 
 if __name__ == "__main__":
     
     try:
         rospy.init_node("move_base_send_goal_node")
         rospy.loginfo("Initcialized: move_base_send_goal_node")
-        s = rospy.Service("move_base/send_goal", Empty, send_goal)
+        s = rospy.Service("move_base/send_goal", GetPlan, send_goal)
         # result = movebase_client()
         # if result:
         #     rospy.loginfo("Goal Execution Done")
